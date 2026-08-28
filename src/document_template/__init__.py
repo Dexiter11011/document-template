3be23b68c058551552e9299da4854db00e6e1db4
dev <dev@local>
2026-08-28 15:59:15 +0300

    Export paths from package __init__

--- src/document_template/__init__.py
+++ src/document_template/__init__.py
@@ -1,5 +1,12 @@
 """document-template: Jinja rendering and style catalogs."""
 
+from document_template.paths import TEMPLATES_DIR, resolve_template
 from document_template.profile import TemplateProfile, default_cache_path, export_gost_styles_config
 
-__all__ = ["TemplateProfile", "default_cache_path", "export_gost_styles_config"]
+__all__ = [
+    "TEMPLATES_DIR",
+    "TemplateProfile",
+    "default_cache_path",
+    "export_gost_styles_config",
+    "resolve_template",
+]
